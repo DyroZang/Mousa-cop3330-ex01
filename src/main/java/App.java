36e666b2f8de9ba0e5168fca088118ca41ce1bd9
Gabriel Mousa <gab.01@hotmail.com>
2021-09-12 20:11:55 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+import java.util.Scanner;
+
+
+public class App {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name +", nice to meet you!");
+
+    }
+}
